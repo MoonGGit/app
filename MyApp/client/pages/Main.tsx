8f7a90e1a6726d5c72719766a8d2e55ccca5990d
Moon <answlgus1122@gmail.com>
2021-07-20 12:27:27 +0900

    css, scss, 부트스트랩 세팅

--- MyApp/client/pages/Main.tsx
+++ MyApp/client/pages/Main.tsx
@@ -1,7 +1,15 @@
-export default function Main(){
-    return(
-        <div>
-            Hello world, React typescript
-        </div>
-    )
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+import a from './test.scss';
+
+export default function Main() {
+	return (
+		<div className={`${a.a}`}>
+			Hello world, React typescript test yeah!
+			<span className={`${a.c}`}>divvvv tag</span>
+			<Button>boot strap test ttt</Button>
+			<Button>boot strap test ttt</Button>
+		</div>
+	);
 }
--- MyApp/client/webpack.config.js
+++ MyApp/client/webpack.config.js
@@ -1,46 +1,83 @@
+const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    mode: prod ? 'production' : 'development',
-    devtool: prod ? 'hidden-source-map' : 'eval',
+	mode: prod ? 'production' : 'development',
+	devtool: prod ? 'hidden-source-map' : 'source-map',
 
-    entry: './public/index.tsx',
+	entry: './public/index.tsx',
 
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx', 'scss', 'css'],
+	},
 
-    module: {
-        rules: [ 
-            {
-                test: /\.tsx?$/,
-                use: ['babel-loader', 'ts-loader'],
-            },
-        ],
-    },
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: ['babel-loader', 'ts-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'@teamsupercell/typings-for-css-modules-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							modules: true,
+							/* 임포트하여 오브젝트로서 사용가능 */
+						},
+					},
+					'sass-loader',
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'@teamsupercell/typings-for-css-modules-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+		],
+	},
 
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js',
-    },
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.js',
+	},
 
-    devServer: {
-        historyApiFallback: true,
-        inline: true,
-        port: 3000,
-        hot: true,
-        publicPath: '/',
-    },
+	devServer: {
+		historyApiFallback: true,
+		inline: true,
+		port: 3030,
+		hot: true,
+		publicPath: '/',
+	},
 
-    plugins: [
-        new webpack.ProvidePlugin({
-            React: 'react',
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-    ],
+	plugins: [
+		new webpack.ProvidePlugin({
+			React: 'react',
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].[contenthash].css',
+			chunkFilename: '[id].[contenthash].css',
+		}),
+	],
 };
